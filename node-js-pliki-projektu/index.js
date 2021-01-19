69576aed6d3edc25d52f786efa17370d5194c0b8
MrSuhar <56634152+MrSuhar@users.noreply.github.com>
2021-01-19 18:25:22 +0100

    Add files via upload
    Plik json z potrzebnymi bibliotekami,szkielet aplikacji (postawienie serwera w Express). Nie zawiera folderu node-modules ze względu na zbyt dużą ilość plików.

---
+++ node-js-pliki-projektu/index.js
@@ -0,0 +1,38 @@
+//Importowanie bibliotek
+const express=require('express');
+
+//ustalenie portu serwera
+const port=8000;
+//środowisko uruchomieniowe
+const env=process.env.NODE_ENV || 'development'; //jesli nie zdefiniowana przyjmuje wartość development
+
+//uruchomienie serwera przez express
+const app=express();
+//Określenie komunikacji z serwerem przy użyciu json'a
+app.use(express.json());
+
+/*
+//Przykładowe zapytanie GET
+app.get('/',(req,res)=>{
+    //res.send({msg:"Witaj w Świecie REST"})
+    res.send(exmpl);
+})
+
+//Przykładowe zapytanie post cz.1
+let exmpl=[];
+
+//Przykładowe zapytanie post cz.2
+app.post('/',(req,res)=>{
+    const {smth_1,smth_2}=req.body;
+    exmpl.push({smth_1,smth_2});
+    //Dodać walidacje
+
+    //Zmiana statusu,wysłanie użytkownikowi obiektu celem sprawdzenia działnia
+    res.status(201).send({smth_1,smth_2})
+})
+*/
+
+//Wypisanie informacji o serwerze w terminalu
+app.listen(port,'localhost',()=>{
+    console.log(`Nasluchuje serwera na adresie http://127.0.0.1:${port} w trybie ${env}`);
+})
